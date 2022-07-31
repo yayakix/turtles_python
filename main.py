c2767a20f9089e69c63c556d27dd073ec85a0a18
Iyana Marquez <yayakix@gmail.com>
2022-07-30 22:56:55 -0400

    random colors generated

--- main.py
+++ main.py
@@ -1,31 +1,32 @@
 import random
-from turtle import Turtle, Screen
+import turtle as t
+t.colormode(255)
 
-turtle = Turtle()
-turtle.shape('turtle')
-turtle.color('green')
-turtle.speed("fast")
+bob = t.Turtle()
+bob.shape('turtle')
+bob.color('green')
+bob.speed("normal")
+bob.pensize(10)
 
 
 def move():
     number = random.randint(5, 70)
-    turtle.forward(number)
+    bob.forward(number)
 def turn():
     number = random.randint(0, 100)
     if number % 2 == 0:
-        turtle.left(90)
+        bob.left(90)
     else:
-        turtle.right(90)
-# def random_color():
-#     r = random.randint(0, 255)
-#     g = random.randint(0, 255)
-#     b = random.randint(0, 255)
-#     random_color = (r, g, b)
-#     return random_color
-colors = ['red','green','blue','pink','yellow','purple']
+        bob.right(90)
+def random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    random_color = (r, g, b)
+    return random_color
 x = 0
 while x < 30:
-    turtle.color(colors[random.randint(0,len(colors) - 1)])
+    bob.color(random_color())
     move()
     turn()
     x += 1
@@ -34,6 +35,5 @@ while x < 30:
 
 
 
-screen = Screen()
-screen.colormode(255)
+screen = t.Screen()
 screen.exitonclick()
